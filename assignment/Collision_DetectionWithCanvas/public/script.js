2db40201bd04562a771dcd1c229c4f080da6da66
SUSHILkhadka <shusilkhadka2@gmail.com>
2022-06-12 19:25:50 +0545

    added collision in canvas

--- assignment/Collision_DetectionWithCanvas/public/script.js
+++ assignment/Collision_DetectionWithCanvas/public/script.js
@@ -1,14 +1,19 @@
-boundWidth = 1200
-boundHeight = 600
+canvasWidth = 1200
+canvasHeight = 600
 
-// boundWidth = 5000
-// boundHeight = 2000
+// canvasWidth = 5000
+// canvasHeight = 2000
 
-bound = document.getElementsByClassName("container")[0];
-bound.style.height = toPx(boundHeight);
-bound.style.width = toPx(boundWidth);
-bound.style.border = "thick solid #0000FF";
-bound.style.position = "relative";
+canvas = document.getElementById("canvas1");
+ctx=canvas.getContext('2d')
+canvas.height = canvasHeight;
+canvas.width = canvasWidth;
+canvas.style.position = 'absolute';
+// canvas.style.top = "100px";
+// canvas.style.left = "100px";
+canvas.style.border = '1px solid #000';
+
+
 
 
 DEFAULT_height = 30;
@@ -18,9 +23,6 @@ DEFAULT_y = 0;
 DEFAULT_dx = 1;
 DEFAULT_dy = 1;
 DEFAULT_color = "#111";
-// DENSITY=100
-// MAX_VELOCITY=15
-
 
 
 var colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", '#345345', '#AB7766'];
@@ -58,31 +60,30 @@ class ball {
         this.m = this.w * this.w;
 
     }
-    create() {
-        this.element = document.createElement('div');
-        this.element.style.width = toPx(this.w);
-        this.element.style.height = toPx(this.h);
 
-        this.element.style.borderRadius = '50%';
-        this.element.style.background = this.color;
-        this.element.style.position = "absolute";
+    create1(){
+        ctx.fillStyle=this.color;
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.w/2,0,Math.PI*2)
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
 
-        bound.appendChild(this.element)
     }
+
+
+
+
+
     move() {
-        // this.x +=this.sx*  this.dx;
-        // this.y +=this.sy* this.dy;
         this.x += this.dx;
         this.y += this.dy;
-
-        this.element.style.top = toPx(this.y)
-        this.element.style.left = toPx(this.x)
     }
     checkWallCollision() {
 
-        if (this.x >= boundWidth - this.w) {
+        if (this.x >= canvasWidth - this.w/2) {
             this.dx = -Math.abs(this.dx);
-            this.x = boundWidth - this.w;
+            this.x = canvasWidth - this.w/2;
         }
 
         if (this.x <= 0) {
@@ -90,9 +91,9 @@ class ball {
             this.x = 0;
         };
 
-        if (this.y >= boundHeight - this.h) {
+        if (this.y >= canvasHeight - this.h/2) {
             this.dy = -Math.abs(this.dy);
-            this.y = boundHeight - this.h;
+            this.y = canvasHeight - this.h/2;
         }
 
         if (this.y <= 0) {
@@ -109,12 +110,12 @@ class ball {
             if (i != index) {
 
 
-                let x1 = this.x + this.w / 2;
-                let y1 = this.y + this.h / 2;
+                let x1 = this.x;
+                let y1 = this.y ;
 
 
-                let x2 = anotherball.x + anotherball.w / 2;
-                let y2 = anotherball.y + anotherball.h / 2;
+                let x2 = anotherball.x 
+                let y2 = anotherball.y;
 
                 let distance = Math.sqrt((x2 - x1) * (x2 - x1) + Math.pow(y2 - y1, 2))
                 let sumofradius = this.w / 2 + anotherball.w / 2;
@@ -140,18 +141,6 @@ class ball {
                     let constant1 = b * (dotx + doty) / unitDivider
                     this.dx = (this.dx - constant1 * normalX)
                     this.dy = (this.dy - constant1 * normalY)
-                    // if(this.dx>MAX_VELOCITY){
-                    //     this.dx=MAX_VELOCITY
-                    // }
-                    // else if(this.dx<-MAX_VELOCITY){
-                    //     this.dx=-MAX_VELOCITY
-                    // }
-                    // if(this.dy>MAX_VELOCITY){
-                    //     this.dy=MAX_VELOCITY
-                    // }
-                    // else if(this.dy<-MAX_VELOCITY){
-                    //     this.dy=-MAX_VELOCITY
-                    // }
 
 
                     let normalX2 = -normalX
@@ -165,28 +154,10 @@ class ball {
                     anotherball.dx = anotherball.dx - constant2 * normalX2
                     anotherball.dy = anotherball.dy - constant2 * normalY2
 
-                    // if(anotherball.dx>MAX_VELOCITY){
-                    //     anotherball.dx=MAX_VELOCITY
-                    // }
-                    // else if(anotherball.dx<-MAX_VELOCITY){
-                    //     anotherball.dx=-MAX_VELOCITY
-                    // }
-                    // if(anotherball.dy>MAX_VELOCITY){
-                    //     anotherball.dy=MAX_VELOCITY
-                    // }
-                    // else if(anotherball.dy<-MAX_VELOCITY){
-                    //     anotherball.dy=-MAX_VELOCITY
-                    // }
-
-
-                    // this.dx = -this.dx
-                    // this.dy = -this.dy
-                    // anotherball.dx=-anotherball.dx
-                    // anotherball.dy=-anotherball.dy
 
 
 
-                    //overlapp removal
+                    // overlapp removal
                     let par = -1
                     if (this.m < anotherball.m) {
                         this.x = anotherball.x - par * sumofradius * normalX / Math.sqrt(unitDivider);
@@ -213,15 +184,15 @@ for (let i = 0; i < 500; i++) {
 
     console.log(generateRandom(0, colors.length))
 
-    let speedfactor = 4;
+    let speedfactor = 3;
     let rdx = generateRandom(-1, 1) * speedfactor;
     let rdy = generateRandom(-1, 1) * speedfactor;
     let rsize = generateRandom(5, 30);
 
 
-    const b = new ball(generateRandom(0 + rsize, boundWidth - rsize),
-    generateRandom(0 + rsize, boundHeight - rsize), rdx, rdy, rsize, colors[Math.floor(generateRandom(0, colors.length))]);
-    b.create();
+    const b = new ball(generateRandom(0, canvasWidth - rsize),
+    generateRandom(0, canvasHeight - rsize), rdx, rdy, rsize, colors[Math.floor(generateRandom(0, colors.length))]);
+    b.create1();
 
     ballarray.push(b);
 
@@ -229,30 +200,22 @@ for (let i = 0; i < 500; i++) {
 
 
 
-function play() {
+
+function animate(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
 
     for (let index = 0; index < ballarray.length; index++) {
+        ballarray[index].create1();
         ballarray[index].move();
         ballarray[index].checkWallCollision();
         ballarray[index].checkBallsCollision(ballarray, index);
     }
 
-    window.requestAnimationFrame(() => {
-        play();
-    })
+
+    requestAnimationFrame(animate);
 }
 
-// function play2() {
+animate();
 
 
-//     for (let index = 0; index < ballarray.length; index++) {
-//         ballarray[index].checkBallsCollision(ballarray, index);
-//     }
-
-//     window.requestAnimationFrame(() => {
-//         play2();
-//     })
-// }
-
-play();
-// play2();
